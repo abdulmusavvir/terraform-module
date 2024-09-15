16aaaa47b27c2068b818ff3d151d45c8083d0cd2
abdulmusavvir <abdulmusavvirrohe@gmail.com>
2024-09-15 18:15:16 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,5 +1,8 @@
 module "mywebserver" {
-  source        = "./modules/webserver"
-  ami_id        = "${var.amiID}"
-  instance_type = "${var.instanceType}"
+  source              = "./modules/webserver"
+  virtualization-type = "${var.virtualization-type}"
+  owner               = "${var.owner}"
+  ami-name            = "${var.ami-name}"
+  root-device-type    = "${var.root-device-type}"
+  instance_type       = "${var.instanceType}"
 }
